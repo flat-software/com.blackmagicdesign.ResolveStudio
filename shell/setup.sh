0031a1dc91aa5d7359c71425ffc7d2e1fbdd0718
Quentin Calvez <quentin.calvez@quentez.com>
2025-06-01 16:12:27 -0700

    Move instead of copy to save space.

--- shell/setup.sh
+++ shell/setup.sh
@@ -19,15 +19,15 @@ mkdir -p ${PREFIX} ${PREFIX}/easyDCP ${PREFIX}/scripts ${PREFIX}/share ${PREFIX}
 chmod 755 ${PREFIX}/easyDCP ${PREFIX}/scripts ${PREFIX}/share ${PREFIX}/Fairlight
 
 # Copy objects.
-cp -rp squashfs-root/bin ${PREFIX}/
-cp -rp squashfs-root/Control ${PREFIX}/
-cp -rp squashfs-root/Certificates ${PREFIX}/
-cp -rp squashfs-root/DaVinci\ Control\ Panels\ Setup ${PREFIX}/
-cp -rp squashfs-root/Developer ${PREFIX}/
-cp -rp squashfs-root/docs ${PREFIX}/
-cp -rp squashfs-root/Fairlight\ Studio\ Utility ${PREFIX}/
-cp -rp squashfs-root/Fusion ${PREFIX}/
-cp -rp squashfs-root/graphics ${PREFIX}/
+mv squashfs-root/bin ${PREFIX}/
+mv squashfs-root/Control ${PREFIX}/
+mv squashfs-root/Certificates ${PREFIX}/
+mv squashfs-root/DaVinci\ Control\ Panels\ Setup ${PREFIX}/
+mv squashfs-root/Developer ${PREFIX}/
+mv squashfs-root/docs ${PREFIX}/
+mv squashfs-root/Fairlight\ Studio\ Utility ${PREFIX}/
+mv squashfs-root/Fusion ${PREFIX}/
+mv squashfs-root/graphics ${PREFIX}/
 
 # Use system libraries instead of the outdated ones provided.
 # https://www.reddit.com/r/Fedora/comments/12z32r1/davinci_resolve_libpango_undefined_symbol_g/
@@ -38,21 +38,21 @@ rm squashfs-root/libs/libgobject*
 # Can we use system Qt5? Not yet.
 # rm squashfs-root/libs/libQt5*
 
-cp -rp squashfs-root/libs ${PREFIX}/
+mv squashfs-root/libs ${PREFIX}/
 
-cp -rp squashfs-root/LUT ${PREFIX}/
-cp -rp squashfs-root/Onboarding ${PREFIX}/
-cp -rp squashfs-root/plugins ${PREFIX}/
-cp -rp squashfs-root/Technical\ Documentation ${PREFIX}/
-cp -rp squashfs-root/UI_Resource ${PREFIX}/
-cp -rp squashfs-root/scripts/script.checkfirmware ${PREFIX}/scripts/
-cp -rp squashfs-root/scripts/script.getlogs.v4 ${PREFIX}/scripts/
-cp -rp squashfs-root/scripts/script.start ${PREFIX}/scripts/
-cp -rp squashfs-root/share/default-config.dat ${PREFIX}/share/
-cp -rp squashfs-root/share/default_cm_config.bin ${PREFIX}/share/
-cp -rp squashfs-root/share/log-conf.xml ${PREFIX}/share/
+mv squashfs-root/LUT ${PREFIX}/
+mv squashfs-root/Onboarding ${PREFIX}/
+mv squashfs-root/plugins ${PREFIX}/
+mv squashfs-root/Technical\ Documentation ${PREFIX}/
+mv squashfs-root/UI_Resource ${PREFIX}/
+mv squashfs-root/scripts/script.checkfirmware ${PREFIX}/scripts/
+mv squashfs-root/scripts/script.getlogs.v4 ${PREFIX}/scripts/
+mv squashfs-root/scripts/script.start ${PREFIX}/scripts/
+mv squashfs-root/share/default-config.dat ${PREFIX}/share/
+mv squashfs-root/share/default_cm_config.bin ${PREFIX}/share/
+mv squashfs-root/share/log-conf.xml ${PREFIX}/share/
 if [[ -e squashfs-root/share/remote-monitoring-log-conf.xml ]]; then
-    cp -rp squashfs-root/share/remote-monitoring-log-conf.xml ${PREFIX}/share/
+  mv squashfs-root/share/remote-monitoring-log-conf.xml ${PREFIX}/share/
 fi
 
 tar -xzvf squashfs-root/share/panels/dvpanel-framework-linux-x86_64.tgz -C ${PREFIX}/libs libDaVinciPanelAPI.so libFairlightPanelAPI.so
